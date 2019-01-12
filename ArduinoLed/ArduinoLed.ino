a31d06f18cfb920018316ceae58ac79c95b6d93c
Luca <luca.bonora@barsanti.edu.it>
2019-01-12 09:54:30 +0100

    Prima Modifica

--- ArduinoLed/ArduinoLed.ino
+++ ArduinoLed/ArduinoLed.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  int LedA = 12;
+  pirMode(LedA, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if digitalRead(LedA, LOW))
+  {
+    digitalRead(LedA, HIGH)
+  }
 }
